2a1db08837b81e6ca804e92706fccf033f029c80
Al Imran <max.imran2001@gmail.com>
2026-02-07 23:05:12 +0600

    refactor: remove unused CSS file and clean up import statements across components

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,52 +1,49 @@
-import { Client, Databases, ID, Query } from 'appwrite';
+import { Client, Databases, ID, Query } from "appwrite";
 
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const TABLE_ID = import.meta.env.VITE_APPWRITE_TABLE_ID;
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 
 const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your Appwrite Endpoint
-    .setProject(PROJECT_ID); // Your project ID
-
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(PROJECT_ID);
 
 const database = new Databases(client);
 
 export const updateSearchCount = async (searchTerm, movie) => {
-    try {
-        const result = await database.listDocuments(DATABASE_ID, TABLE_ID, [
-            Query.equal('searchTerm', searchTerm)
-        ]);
+  try {
+    const result = await database.listDocuments(DATABASE_ID, TABLE_ID, [
+      Query.equal("searchTerm", searchTerm),
+    ]);
 
-        if (result.documents.length > 0) {
-            const doc = result.documents[0];
-            await database.updateDocument(DATABASE_ID, TABLE_ID, doc.$id, {
-                count: doc.count + 1
-            });
-        } else {
-            await database.createDocument(DATABASE_ID, TABLE_ID, ID.unique(), {
-                searchTerm,
-                count: 1,
-                movie_id: movie.id,
-                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-            });
-        }
+    if (result.documents.length > 0) {
+      const doc = result.documents[0];
+      await database.updateDocument(DATABASE_ID, TABLE_ID, doc.$id, {
+        count: doc.count + 1,
+      });
+    } else {
+      await database.createDocument(DATABASE_ID, TABLE_ID, ID.unique(), {
+        searchTerm,
+        count: 1,
+        movie_id: movie.id,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
     }
-    catch (error) {
-        console.error('Error updating search count:', error);
-    }
-}
+  } catch (error) {
+    console.error("Error updating search count:", error);
+  }
+};
 
 export const getTrendingMovies = async () => {
-    try {
-        const result = await database.listDocuments(DATABASE_ID, TABLE_ID, [
-            Query.orderDesc('count'),
-            Query.limit(10)
-        ]);
+  try {
+    const result = await database.listDocuments(DATABASE_ID, TABLE_ID, [
+      Query.orderDesc("count"),
+      Query.limit(10),
+    ]);
 
-        return result.documents;
-    }
-    catch (error) {
-        console.error('Error fetching trending movies:', error);
-        return [];
-    }
-}
+    return result.documents;
+  } catch (error) {
+    console.error("Error fetching trending movies:", error);
+    return [];
+  }
+};
